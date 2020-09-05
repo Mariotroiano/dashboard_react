b1bade148a3efb1b96461c3da4005c5bed649002
Mario Troiano <mariotroiano2@gmail.com>
2020-09-05 14:04:34 -0300

    cambio de color al clickear

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,18 +4,34 @@ import React, { Component } from 'react'
 class Category extends Component {
     constructor(props) {
         super(props)
-    }
-    
-    changeColor() {
-        document.querySelector("#change-color").style.backgroundColor = "gold"
-        document.querySelector("#change-color").style.border = "gold"
+        this.state = {
+            bgColor : '',
+            border : ''
+
+        }
+    }    
+
+    changeColor = (e)=> {
+        if(this.state.bgColor == ""){
+            this.setState({
+                bgColor : 'gold',
+               
+            })
+        }else{
+            this.setState({
+                bgColor : '',
+                
+            })
+        }
+      
+       
     }
 
     render() {
         return (
             <div className="col-lg-6 mb-4">
                 <div className="card bg-info text-white shadow">
-                    <div className="card-body"  id="change-color"  onClick={ this.changeColor }>
+                    <div className="card-body change" onClick={ this.changeColor } style={{backgroundColor : this.state.bgColor}}  >
                         {this.props.item}
                     </div>
                 </div>
